fb384e8b95aaac0724a999ff86a34185ea94a63a
mayankamencherla <mayankamencherla24@gmail.com>
2018-08-11 04:00:53 +0530

    [image upload] Saving image url in blog DB

--- routes/uploadRoutes.js
+++ routes/uploadRoutes.js
@@ -14,7 +14,7 @@ module.exports = app => {
 
     s3.getSignedUrl('putObject', {
       Bucket: 'my-blog-mayankamencherla-bucket-123',
-      ContentType: 'jpeg',
+      ContentType: 'image/jpeg',
       Key: key
     }, (err, url) => res.send({key, url}));
   });
